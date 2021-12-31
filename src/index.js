83c1d778e66715240020e9cc7baa35153cf02128
Daniel Garcia <dgarcia@grupomabarak.mx>
2021-12-31 13:01:23 -0600

    introducción a react

--- src/index.js
+++ src/index.js
@@ -11,6 +11,32 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// const X = () => <p>Hola Mundo</p>
+// const Li = ({ children, estado, casa, edad }) =>{
+//   console.log(casa,edad)
+//   return <li> {children} {estado} </li>
+// } 
+// const X = () => 
+//   <ul>
+//     <Li 
+//       estado={'Feliz'}
+//       casa = {false}
+//       edad = {24}
+//     >
+//       Chachito
+//     </Li>
+//     <Li
+//       estado={'Trite'}
+//       casa = {true}
+//       edad = {24}
+//     >Chachito</Li>
+//     <li>Felipe</li>
+//   </ul>
+
+// ReactDOM.render(
+//   <X/>, document.getElementById('root')
+// );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
